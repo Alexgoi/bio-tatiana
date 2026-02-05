233c2fd246481d40446d4a67eb063a442beba1db
Alexsandro Goi <alexmoraisgoi@gmail.com>
2026-02-05 09:39:07 -0300

    link do livro colocado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 // Adicionei o FaCheck de volta e removi o FaMapMarkerAlt que não estava sendo usado
 import { FaWhatsapp, FaEnvelope, FaInstagram, FaLinkedinIn, FaGlobe, FaFacebook, FaYoutube, FaCheck, FaUser, FaUserFriends } from 'react-icons/fa';
+import { IoBookSharp } from "react-icons/io5";
 
 function App() {
   const [copied, setCopied] = useState(false);
@@ -21,29 +22,29 @@ function App() {
 
       // No celular, tenta abrir o app de e-mail (opcional, mas recomendado)
       if (/Mobi|Android/i.test(navigator.userAgent)) {
-         window.location.href = `mailto:${email}`;
+        window.location.href = `mailto:${email}`;
       }
     });
   };
 
   return (
     <div className="layout">
-      
+
       {/* Títulos */}
       <h1>{profile.name}</h1>
       <p className="subtitle">{profile.title}</p>
-      
+
       {/* Lista de Links */}
       <nav className="link-list">
-        
+
         <a href="https://wa.me/5551991160078?text=Ol%C3%A1,%20gostaria%20de%20conversa%20com%20a%20equipe%20da%20Dra.%20Tatiana%20Lima" className="minimal-link" target="_blank" rel="noopener noreferrer">
-          <FaWhatsapp className="icon" /> 
+          <FaWhatsapp className="icon" />
           <span>Agendar Consulta</span>
         </a>
 
         {/* Botão de E-mail Inteligente (Copia ao clicar) */}
-        <button onClick={handleEmailClick} className="minimal-link" style={{fontFamily: 'inherit', background: 'transparent', border: 'none', cursor: 'pointer', width: '100%'}}>
-          {copied ? <FaCheck className="icon" style={{color: '#4caf50'}} /> : <FaEnvelope className="icon" />}
+        <button onClick={handleEmailClick} className="minimal-link" style={{ fontFamily: 'inherit', background: 'transparent', border: 'none', cursor: 'pointer', width: '100%' }}>
+          {copied ? <FaCheck className="icon" style={{ color: '#4caf50' }} /> : <FaEnvelope className="icon" />}
           <span>{copied ? "E-mail Copiado!" : "Enviar E-mail"}</span>
         </button>
 
@@ -53,8 +54,12 @@ function App() {
         </a>
 
         <a href="https://chat.whatsapp.com/GnPelkWzkThA1uuulN5aGv" className="minimal-link" target="_blank" rel="noopener noreferrer">
-            <FaUserFriends className="icon" />
-            <span>Comunidade Escola Veritas</span>
+          <FaUserFriends className="icon" />
+          <span>Comunidade Escola Veritas</span>
+        </a>
+        <a href="https://produto.mercadolivre.com.br/MLB-6224416342-livro-assembleias-condominiais-desafios-e-solucoes-para-uma-gesto-eficaz-tatiana-lima-_JM" className="minimal-link" target="_blank" rel="noopener noreferrer">
+          <IoBookSharp className="icon" />
+          <span>Livro Assembleias Condominiais</span>
         </a>
 
       </nav>
